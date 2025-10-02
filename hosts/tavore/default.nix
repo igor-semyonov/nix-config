26b4b10b4e0ed68dc6d9a2249f090ff315818709
Igor Semyonov <igor@semyonov.xyz>
2025-10-01 23:25:31 -0400

    added openwebui

--- hosts/tavore/default.nix
+++ hosts/tavore/default.nix
@@ -19,11 +19,49 @@
     "${nixosModules}/hardware/nas"
   ];
 
-  services.ollama = {
-    enable = true;
-    models = "/mnt/ollama-models";
-    openFirewall = true;
-    host = "0.0.0.0";
+  services = {
+    ollama = {
+      enable = true;
+      models = "/mnt/ollama-models";
+      openFirewall = true;
+      host = "0.0.0.0";
+      port = 11434;
+    };
+    open-webui = {
+      enable = true;
+      openFirewall = true;
+      host = "0.0.0.0";
+      port = 11435;
+      environment = let
+        t = "True";
+        f = "False";
+      in {
+        ANONYMIZED_TELEMETRY = f;
+        DO_NOT_TRACK = t;
+        SCARF_NO_ANALYTICS = t;
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+
+        ENABLE_SIGNUP = t;
+        DEFAULT_USER_ROLE = "pending";
+        DEFAULT_LOCALE = "en";
+        ENABLE_CHANNELS = t;
+        THREAD_POOL_SIZE = "64";
+        MODELS_CACHE_TTL = "0";
+        ENABLE_REALTIME_CHAT_SAVE = t;
+        BYPASS_MODEL_ACCESS_CONTROL = t;
+        ENABLE_TITLE_GENERATION = t;
+        DEFAULT_PROMPT_SUGGESTIONS = ''
+          [
+            {
+              "title": [
+                "Title part 1",
+                "Title part 2"
+              ],
+              "content": "prompt"
+            }
+          ]'';
+      };
+    };
   };
 
   nix.settings = {
