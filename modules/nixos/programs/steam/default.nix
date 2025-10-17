6117ac208c102a52cd5d430d399ed87c206ccd81
Igor Semyonov <igor@semyonov.xyz>
2025-10-16 23:04:26 -0400

    added steam options

--- modules/nixos/programs/steam/default.nix
+++ modules/nixos/programs/steam/default.nix
@@ -1,7 +1,27 @@
-{...}: {
+{
+  pkgs,
+  userConfig,
+  ...
+}: {
   # Steam gaming platform configuration
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        MANGOHUD = true;
+        OBS_VKCAPTURE = true;
+        RADV_TEX_ANISO = 16;
+      };
+      extraLibraries = p:
+        with p; [
+          atk
+        ];
+    };
     remotePlay.openFirewall = true;
+    extest.enable = true;
   };
+  users.users.${userConfig.name}.packages = with pkgs; [
+    protonup-qt
+    mangohud
+  ];
 }
