c9ab23b7e68486c30c08da5772aed0a418449445
Igor Semyonov <igor@semyonov.xyz>
2025-11-30 23:02:33 -0500

    removed firefox overrideAttrs in hopes of speeding up builds

--- modules/nixos/programs/firefox/default.nix
+++ modules/nixos/programs/firefox/default.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}: {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox.overrideAttrs (oldAttrs: {
-      disallowedRequisites = [];
-    }); # firefox-144 did not build without this
+    # package = pkgs.firefox.overrideAttrs (oldAttrs: {
+    #   disallowedRequisites = [];
+    # }); # firefox-144 did not build without this
     policies.SecurityDevices.p11-kit-proxy = "${pkgs.p11-kit}/lib/p11-kit-proxy.so";
   };
 
